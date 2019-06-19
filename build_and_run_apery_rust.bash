3f71390433bf44c5f01afbdf36bc64ffcb0163fb
HiraokaTakuya <hiraoka64@gmail.com>
2019-06-19 13:10:04 +0900

    Fix the code

--- build_and_run_apery_rust.bash
+++ build_and_run_apery_rust.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 if [ -e apery_rust ]; then
-	git clone https://github.com/HiraokaTakuya/apery_rust.git
-	cd apery_rust
-	git submodule init
-	git submodule update
-	cargo build --release
-	./target/release/apery <<- EOF
-	setoption name Threads value 1
-	isready
-	go byoyomi 10000
-	wait
-	EOF
+    :
+else
+    git clone https://github.com/HiraokaTakuya/apery_rust.git
 fi
+cd apery_rust
+git submodule init
+git submodule update
+cargo build --release
+./target/release/apery <<- EOF
+setoption name Threads value 1
+isready
+go byoyomi 10000
+wait
+EOF
